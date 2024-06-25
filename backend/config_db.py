f86e30d15e52613953cd45b2a1a81b1765c56197
agusNazer <agustin.nazer@hotmail.com>
2024-06-25 00:41:42 -0300

    dos strings

--- backend/config_db.py
+++ backend/config_db.py
@@ -9,23 +9,18 @@ load_dotenv()
 
 # Obtener las variables de entorno
 DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PASSWORD= os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
 # Crear la cadena de conexión
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# DATABASE_URL = "postgresql://postgres:Alma2022@localhost:5432/esq1.0"
+DATABASE_URL = "postgresql://postgres:Alma2022@localhost:5432/esq1.0"
 
-# Crear el motor de la base de datos
+
 engine = create_engine(DATABASE_URL)
-print(DB_HOST)
-print(f"DB_USER: {DB_USER}")
-print(f"DB_PASSWORD: {DB_PASSWORD}")
-print(f"DB_HOST: {DB_HOST}")
-print(f"DB_PORT: {DB_PORT}")
-print(f"DB_NAME: {DB_NAME}")
+
 
 # Crear una sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
