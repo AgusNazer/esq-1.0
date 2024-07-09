cf6f4acb55bcff6888876337ac5804564d4cb273
agusNazer <agustin.nazer@hotmail.com>
2024-07-09 20:07:16 -0300

    login jwt

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,71 @@ from flask import Flask
 from src.routes.user import users
 from config_db import engine
 from src.models.user import Base
+from flask import Flask, Blueprint, jsonify, request, make_response, render_template, session
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
+from src.auth.auth import auth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
 app.register_blueprint(users, url_prefix='/users')
+app.register_blueprint(auth, url_prefix='/auth')
+
 
 Base.metadata.create_all(bind=engine)
 
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+
+# def token_required(func):
+#     @wraps(func)
+#     def decorated(*args, **kwargs):
+#         token = request/args.get('token')
+#         if not token:
+#             return jsonify({'Alert!': 'Token is missing'})
+#         try:
+#             payload = jwt.decode(token, app.config['SECRET_KEY'])
+#         except:
+#             return jsonify({'Alert': 'Invalid Token!'})
+#     return decorated
+
+# #home
+# @app.route('/home')
+# def home():
+#     if not session.get('logged_in'):
+#         return render_template('login.html')
+#     else:
+#         return 'Logged in currently!'
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     if request.form['username'] and request.form['password'] == '123456':
+#         session['logged_in'] = True
+#         token = jwt.encode({
+#             'user': request.form['username'],
+#             'expiration': str(datetime.utcnow() + timedelta(seconds=120))
+#         }, app.config['SECRET_KEY'], algorithm="HS256")
+#         return jsonify({'token': token})
+#     else:
+#         return make_response('Unable to verify', 403, {'WWW.Authenticate' : 'Basic realm: "Authentication Failed!'})
+
+
+# @app.route('/public')
+# def public():
+#     return 'For public'
+
+# #auth
+# @app.route('/auth')
+# @token_required
+# def auth():
+#     return 'JWT is verified. Welcome to your dashboard!'
 
 
 @app.route('/')
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,12 +1,58 @@
-from flask import Blueprint, jsonify, request
+# src/auth/auth.py
+from flask import Blueprint, jsonify, request, make_response, render_template, session, current_app, redirect, url_for
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
 
 auth = Blueprint('auth', __name__)
 
-base_url = '/src/auth/'
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'Alert!': 'Token is missing'}), 403
+        try:
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'Alert': 'Token has expired!'}), 403
+        except jwt.InvalidTokenError:
+            return jsonify({'Alert': 'Invalid Token!'}), 403
+        return func(*args, **kwargs)
+    return decorated
 
-@auth.route(base_url + 'issue_token', methods=['POST'])
-def issue_token():
-    try:
-        user_data = request.get_json()
-    except Exception as e:
-        return jsonify({'result': False, 'response': f'Error en la solicitud: {e}'}), 400
+@auth.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] and request.form['password'] == '123456':
+        session['logged_in'] = True
+        token = jwt.encode({
+            'user': request.form['username'],
+            'expiration': str(datetime.utcnow() + timedelta(seconds=120))
+        }, current_app.config['SECRET_KEY'], algorithm="HS256")
+        return jsonify({'token': token})
+    else:
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm="Authentication Failed!"'})
+         
+@auth.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    # return jsonify({'message': 'Successfully logged out'}), 200return redirect(url_for('auth.home'))
+
+    return redirect(url_for('auth.home'))
+
+
+@auth.route('/home')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return 'Logged in currently!'
+
+@auth.route('/public')
+def public():
+    return 'For public'
+
+@auth.route('/auth')
+@token_required
+def auth_route():
+    return 'JWT is verified. Welcome to your dashboard!'
